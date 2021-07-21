ce96498aed63aad0009b64ca9ccb3e2d63174281
yutarou12 <sasaki.yuutarou12@gmail.com>
2021-07-21 10:46:15 +0900

    refactoring: 役職情報のコマンドを改修

--- cogs/info.py
+++ cogs/info.py
@@ -1,5 +1,4 @@
 import asyncio
-import re
 import discord
 from discord.ext import commands
 from pytz import timezone
@@ -9,7 +8,6 @@ class Info(commands.Cog):
     """チャンネルなどの情報を見るためなどのコマンドがあるカテゴリーです"""
     def __init__(self, bot):
         self.bot = bot
-        self.role_info = None
         self.user_info = None
 
     @commands.command(description='サーバーの情報を表示します',
@@ -74,30 +72,12 @@ class Info(commands.Cog):
                              '・ID: {cmd}roleinfo 123456789012345678\n'
                              '・メンション: {cmd}roleinfo <@&123456789012345678>\n'
                              '・名前: {cmd}roleinfo Bot'])
-    async def roleinfo(self, ctx, role=None):
+    async def roleinfo(self, ctx, role: discord.Role = None):
         if role is None:
             no_role_msg = discord.Embed(description='役職を以下の形で指定してください\n```\n・ID\n・名前\n・メンション\n```')
             return await ctx.reply(embed=no_role_msg, allowed_mentions=discord.AllowedMentions.none())
-
-        elif ctx.message.role_mentions:
-            self.role_info = ctx.message.role_mentions[0]
-        elif re.search(r'[0-9]{18}', str(role)) is not None:
-            pre_role = ctx.guild.get_role(int(role))
-            if pre_role:
-                self.role_info = pre_role
-            else:
-                no_role_msg = discord.Embed(description='役職が見つかりませんでした\n**考えられる原因**```\nIDは間違っていませんか？\n```')
-                return await ctx.reply(embed=no_role_msg, allowed_mentions=discord.AllowedMentions.none())
         else:
-            pre_role = discord.utils.get(ctx.guild.roles, name=role)
-            if pre_role:
-                self.role_info = pre_role
-            else:
-                no_role_msg = discord.Embed(description='役職が見つかりませんでした\n**考えられる原因**```\n名前は間違っていませんか？\n```')
-                return await ctx.reply(embed=no_role_msg, allowed_mentions=discord.AllowedMentions.none())
-
-        if self.role_info is not None:
-            role_info = self.role_info
+            role_info = role
             role_id = role_info.id
             role_name = role_info.name
             role_created = role_info.created_at
@@ -109,12 +89,12 @@ class Info(commands.Cog):
             role_member = ''
             if len(role_members) == 0:
                 role_member += 'なし'
-            elif len(role_members) < 10:
+            elif len(role_members) < 15:
                 for m in range(len(role_members)):
                     role_member += role_members[m].mention + ', '
             else:
-                role_member += '`上位10人を表示`\n'
-                for m in range(10):
+                role_member += '`上位15人を表示`\n'
+                for m in range(15):
                     role_member += role_members[m].mention + ', '
                 role_member += ' ...以下略'
 

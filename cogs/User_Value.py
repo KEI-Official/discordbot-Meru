dea411b995fbd6087f0677b3bc7bd53d97f96a99
yutarou12 <sasaki.yuutarou12@gmail.com>
2021-08-01 15:30:34 +0900

    add: Ban時にスコアが減少する仕組みを追加

--- cogs/User_Value.py
+++ cogs/User_Value.py
@@ -50,6 +50,31 @@ class User_Value(commands.Cog):
             if res:
                 return await ctx.reply('スコアデータをリセットしました', allowed_mentions=discord.AllowedMentions.none())
 
+    @commands.Cog.listener()
+    async def on_member_ban(self, guild, user):
+        res = self.db.user_evaluation_get(user.id)
+        if res:
+            old_ban_count = res[0][2]
+            old_score = res[0][2]
+            reason = res[0][3]
+            new_ban_count = 1 + int(old_ban_count)
+            if new_ban_count == 3:
+                new_score = int(old_score) - 1
+            elif new_ban_count == 5:
+                new_score = int(old_score) - 1.5
+            elif new_ban_count > 7:
+                new_score = int(old_score) - 1.5
+
+            check = self.db.user_evaluation_update(user.id, str(new_score), new_ban_count, reason)
+            if check:
+                owner = await self.bot.fetch_user((await self.bot.application_info()).owner.id)
+                return await owner.send(f'{user} が {guild} にてBanされたため、スコアが {new_score}　になりました')
+        else:
+            check = self.db.user_evaluation_set(user.id, '9.0', '1', '[メル]Banによる減少')
+            if check:
+                owner = await self.bot.fetch_user((await self.bot.application_info()).owner.id)
+                return await owner.send(f'{user} が {guild} にてBanされたため、スコアが 9.0　になりました')
+
 
 def setup(bot):
     bot.add_cog(User_Value(bot))
